24a0b3c7972da8a1d2b3afd86a3878c2578b65e5
Kriator7 <62114893+Kriator7@users.noreply.github.com>
2022-11-17 12:29:11 -0800

    Add project files.

---
+++ ModernDesign2/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ModernDesign2.Core;
+
+
+namespace ModernDesign2.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObjects
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _curentView;
+
+        public object CurrentView
+        {
+            get { return _curentView; }
+            set 
+            {
+                _curentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel() 
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
